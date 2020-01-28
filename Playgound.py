29b8ab5d6ad1590165288a39f37567c445525966
Titus Pahn <TitusPahn2019@u.northwestern.edu>
2020-01-27 23:06:45 -0600

    hm

--- Playgound.py
+++ Playgound.py
@@ -1,8 +1,15 @@
+import json
 import nltk
+from nltk.corpus import stopwords
+
+with open('gg2020.json', 'rb') as f:
+    data = f.readlines()
+
 
 if __name__ == "__main__":
     sentence = "Hello World"
     tokens = nltk.word_tokenize(sentence)
     print("Hello World")
     print(tokens)
+
 
