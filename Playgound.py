debe5d72e364e4c51d87526d436f13fc53329b51
Titus Pahn <TitusPahn2019@u.northwestern.edu>
2020-01-27 23:14:15 -0600

    hm2

--- Playgound.py
+++ Playgound.py
@@ -2,14 +2,25 @@ import json
 import nltk
 from nltk.corpus import stopwords
 
-with open('gg2020.json', 'rb') as f:
+with open('gg2020.json', 'r') as f:
     data = f.readlines()
 
+stop_words = set(stopwords.words('english'))
 
 if __name__ == "__main__":
     sentence = "Hello World"
     tokens = nltk.word_tokenize(sentence)
     print("Hello World")
     print(tokens)
+    print(data[0])
+    whatever = data[0]
+    data_tokens = nltk.word_tokenize(whatever)
 
+    unstopped_sentence = []
+
+    for t in data_tokens:
+        if t not in stop_words:
+            unstopped_sentence.append(t)
+
+    print(unstopped_sentence)
 
