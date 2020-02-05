243fa701b2ecc7e1c894f7478017c83a58a896cf
ThomasBarnett <38731315+ThomasBarnett@users.noreply.github.com>
2020-02-04 21:37:40 -0600

    Small Change

--- Playgound.py
+++ Playgound.py
@@ -3,6 +3,7 @@ import nltk
 if __name__ == "__main__":
     sentence = "Hello World"
     tokens = nltk.word_tokenize(sentence)
+    tagged = nltk.pos_tag(tokens)
     print("Hello World")
-    print(tokens)
+    print(tagged)
 
