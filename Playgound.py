25ac0bd6968d542ec9a1f216af40fee1e87e2ba8
ThomasBarnett <38731315+ThomasBarnett@users.noreply.github.com>
2020-02-06 19:59:38 -0600

    Redid finding objects
    Current Idea: Use subject to compare Objects to pare down useless words ???. This is giving us a lot of the information we need though

--- Playgound.py
+++ Playgound.py
@@ -1,6 +1,7 @@
 import json
 import nltk
 from nltk.corpus import stopwords
+from nltk.tree import *
 
 with open('gg2013.json') as json_file:
     data = json.load(json_file)
@@ -20,6 +21,8 @@ def unstop():
 
 
 def unhashtag(str):
+    #if "RT " in str:
+    #    str = str[str.index(':'):]
     return str.replace('#', '')
 
 
@@ -28,13 +31,25 @@ def wordIndexInTree(tree, word):
         if tree[i][0] == word:
             return i
 
+
 def objectSearch(tree, index):
-    for i in range(index, len(tree)):
-        if "NNP" in tree[i][0]:
-            object = ""
-            for word_pair in tree[i]:
-                object = object + word_pair[0]
-            return object
+    searching = True
+    compiling = True
+    object = ""
+
+    while searching and index < len(tree):
+        if "Best" == tree[index][0]:
+            searching = False
+            while index < len(tree):
+                if False:
+                    compiling = False
+                    break
+                object = object + tree[index][0] + ' '
+                index = index + 1
+        index = index+1
+
+    return object
+
 
 def subjectSearch(tree, index):
     for i in range(index, 0, -1):
@@ -46,14 +61,17 @@ def subjectSearch(tree, index):
 
 
 def buildRelation(text, verb):
+
     text = unhashtag(text)
     tokens = nltk.word_tokenize(text)
     tagged = nltk.pos_tag(tokens)
     tree = nltk.chunk.ne_chunk(tagged)
 
-    windex = wordIndexInTree(tree, verb)
-    obj = objectSearch(tree, windex)
-    subj = subjectSearch(tree, windex)
+    obj = objectSearch(tagged, wordIndexInTree(tagged, verb))
+    subj = subjectSearch(tree, wordIndexInTree(tree, verb))
+    if obj is "" or subj is None:
+        return None
+
     return Relation(obj, verb, subj)
 
 
@@ -73,16 +91,59 @@ if __name__ == "__main__":
 
     i = 0
     winTweets = []
-    while len(winTweets) < 50:
+    while len(winTweets) < 500:
         if " won " in data[i]['text']:
             winTweets.append(data[i])
-        i=i+1
+        i = i+1
 
-    print(winTweets[26])
-    text = winTweets[26]['text']
+    """
+    text = unhashtag(
+        "Christoph Waltz won a Golden Globe for Best Supporting Actor In A Motion Picture! Congrats! #GoldenGlobes")
+    tokens = nltk.word_tokenize(text)
+    tagged = nltk.pos_tag(tokens)
+    tree = nltk.chunk.ne_chunk(tagged)
+    tree = nltk.chunk.ne_chunk(tagged)
+    print(tagged)
+    print(tree)
 
-    relation = buildRelation(text, "won")
-    relation.display()
+    #buildRelation(text, "won")
+
+    
+    text = unhashtag("RT @PimpBillClinton: I just won the Golden Globe for Acting (Like I Give A Shit About Whatever My Wife Is Talking About). #GoldenGlobes")
+    tokens = nltk.word_tokenize(text)
+    tagged = nltk.pos_tag(tokens)
+    tree = nltk.chunk.ne_chunk(tagged)
+    print(tree)
+    """
+
+    cleanRelation = []
+
+    for i in range(0, len(winTweets)):
+        text = winTweets[i]['text']
+        relation = buildRelation(text, "won")
+
+        #print(str(i) + ": " + text)
+
+        if relation is not None:
+            cleanRelation.append(relation)
+
+    for relation in cleanRelation:
+        relation.display()
+
+    """ 
+    for i in range(0, len(winTweets)):
+        text = winTweets[i]['text']
+        relation = buildRelation(text, "won")
+
+        print(str(i) + ": " + text)
+
+        if relation is None:
+            print("NONE")
+        else:
+            relation.display()
+            """
+
+    #relation.display()
 
 
 
