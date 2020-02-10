92fe105392d00daf8a4fdd278ef4f342c17a582b
alexbanta4 <alexbanta4@gmail.com>
2020-02-09 18:19:36 -0600

    More work on Awards
    Made multiple changes to award words list and filtering technique, also revamped word combination. All awards are present, but list is too large; frequency-motivated similar word removal will be added - Alex Banta

--- Playgound.py
+++ Playgound.py
@@ -6,7 +6,17 @@ from fuzzywuzzy import fuzz
 #Githubs I've looked at:
 # Brownrout
 # LJGladic (PMA alum so his code must be good)
+
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+
+#A comprehensive list of any words that indicate that a certain phrase is an award.
+award_words = ['Best','Motion','Picture','Drama','Musical','Comedy','Performance','Actor','Actress','Supporting','Leading','Role',
+               'Director','Movie','Film','Feature','Screenplay','Animated','Foreign','Language','Original','Song','Score',
+               'Television','TV','Series','Mini','Mini-Series','Limited']
+award_words_lower = [word.lower() for word in award_words]
+#A comprehensive list of any words that appear in an award but don't indicate that it is an award
+helper_words = ['By','In','For','A','An','-',':','Or','Made','Any']
+helper_words_lower = [word.lower() for word in helper_words]
 
 with open('gg2013.json') as json_file:
     data = json.load(json_file)
@@ -76,7 +86,7 @@ def buildRelation(text, verbs):
     [tagIndex, verb] = wordIndexInTree(tagged,verbs)
     obj = objectSearch(tagged, tagIndex)
     subj = subjectSearch(tree, wordIndexInTree(tree, verbs)[0])
-    if obj is "" or subj is None:
+    if obj == "" or subj is None:
         return None
 
     return Relation(obj, verb, subj)
@@ -108,7 +118,8 @@ class Award:
                 max = people_comb[person]
                 winner = person
 
-        print(winner + "won " + self.title)
+        #print(winner + "won " + self.title)
+        return winner
 
 
 
@@ -147,17 +158,17 @@ class VoteBoard:
 
 
 def get_awards(year):
-    award_words = ['Best','Motion','Picture','Drama','Musical','Comedy','Performance','Actor','Actress','Supporting','Director','Film','Feature','Screenplay','Animated','Foreign','Language','Original','Song','Television','Series','Mini'] # Mini is for 'mini-series'
-    award_words_lower = [word.lower() for word in award_words]
-    helper_words = ['By','In','For','A','An','-',':','Or','Made'] #TODO: remove 'for' if it's the last word in the award
-    helper_words_lower = [word.lower() for word in helper_words]
 
     potential_award_tweets = [] # Look for tweets with award words in them
     for tweet in data:
-        tweet = tweet['text'].strip(',').strip(':').strip('!').strip('.').strip('?')
+        # Punctuation and extra spaces r bad so purge it aggressively unless it is a hyphen
+        tweet = tweet['text'].replace('  ',' ')
+        for punct in [',',':','!','.','?']:
+            tweet = tweet.replace(punct,'')
+        #Find the award words
         award_words_count = 0
-        for token in tweet.split(' '):
-            if token.lower() in award_words_lower:
+        for token in tweet.lower().split(' '):
+            if token in award_words_lower:
                 award_words_count += 1
         if award_words_count >= 3:
             potential_award_tweets.append(tweet)
@@ -176,6 +187,8 @@ def get_awards(year):
         award_name = ""
         while (not at_end_award):
             word = award_tokens[award_ind]
+            if word == 'tv':
+                word = 'television'
             if word.lower() in award_words_lower or word in helper_words_lower:
                 award_name += word + " "
             else:
@@ -193,24 +206,39 @@ def get_awards(year):
 
     # Frequency voting and process partial awards
 
-    awards = [award.replace(" -","") for award in awards]
+    #awards = [award.replace(" -","") for award in awards]
 
     return set(awards)
 
 
 def combine_awards(awards_list):
     awards_comb = {}
+    awards_comb_length = {} # This dictionary exists only to save time by reducing the number of iterations through the array
     awards_list = sorted(awards_list, key=len, reverse=True)
 
-    for tweet in awards_list:
+    for award in awards_list:
+        award_word_list = [w for w in award.split(' ') if w in award_words_lower]
+        award_word_count = len(award_word_list)
         found = False
         for title in awards_comb:
-            if tweet in title:
-                awards_comb[title] = awards_comb[title] + 1
+            found_word_count = 0
+            for word in award.split(' '):
+                if word not in award_words_lower:
+                    continue
+                if word in title:
+                    found_word_count += 1
+
+            if found_word_count == award_word_count: #If every award word in award is in title, they could be the same award
                 found = True
-                break
+                awards_comb[title] = awards_comb[title] + 1
+
         if not found:
-            awards_comb[tweet] = 1
+            awards_comb[award] = 1
+            awards_comb_length[award] = award_word_count
+
+    # Start using those frequencies, with some fuzzy matching as well
+    #for award_name in awards_comb.keys():
+    #    if awards_comb[award_name] >
 
     return awards_comb
 
@@ -272,6 +300,7 @@ def people_in_tweet(tree):
 
 if __name__ == "__main__":
 
+    print('Finding people...')
     i = 0
     hostTweets = []
     winTweets = []
@@ -291,6 +320,7 @@ if __name__ == "__main__":
 
     cleanRelation = []
 
+    print('Building relations... (This may take a while)')
     for i in range(0, len(winTweets)):
         text = winTweets[i]['text']
         relation = buildRelation(text, winWords)
@@ -314,10 +344,11 @@ if __name__ == "__main__":
 
 
 
-
+    print('Finding Awards...')
     awards = get_awards('2013')
     compiled_awards = combine_awards(awards)
-    print(compiled_awards)
+    #for key,value in compiled_awards.items():
+    #    print(key + ": " + str(value))
 
     award_array = []
     for award in OFFICIAL_AWARDS_1315:
@@ -350,7 +381,6 @@ if __name__ == "__main__":
 #Alex TODO
 #Finding awards:
 # 1. At some point convert everything to lowercase
-# 2. Write regex award finder because I don't trust relations
 # 3. Replace combiner to use closest fuzz logic and not in
 
 #Finding winners:
